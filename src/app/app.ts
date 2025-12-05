964f53dcf3113f118c0921bd6b05c76959ea6612
wrincied <wrincied@gmail.com>
2025-12-05 21:57:38 +0100

    Add new Service, Update App.ts, refactoring

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,46 @@
-import { Component, signal } from '@angular/core';
+import { Component, Inject, NgZone, PLATFORM_ID, signal } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet, RouterLinkWithHref, RouterLink } from '@angular/router';
 import { TopNav } from "./components/top-nav/top-nav";
+import { isPlatformBrowser } from '@angular/common';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule, TopNav],
+  imports: [RouterOutlet, ReactiveFormsModule, TopNav, RouterLinkWithHref, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('spotify-clone');
+  constructor(
+    private router: Router,
+    private ngZone: NgZone,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { }
+
+  ngOnInit(): void {
+    if (!isPlatformBrowser(this.platformId)) return;
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.runGlobalUiUpdates();
+      });
+  }
+
+  private runGlobalUiUpdates(): void {
+    this.ngZone.runOutsideAngular(() => {
+
+      // Пример: переинициализация slider
+
+
+      // Пример: скролл вверх при смене страницы
+      document.querySelector('.spotify-main')?.scrollTo(0, 0);
+    });
+  }
+  showLayout(): boolean {
+    return !this.router.routerState.snapshot.root.firstChild?.data['noLayout'];
+  }
 }
+
+
